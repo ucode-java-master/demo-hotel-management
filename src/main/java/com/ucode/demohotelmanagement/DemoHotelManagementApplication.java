44364e1028c8ea54d571602a92e2ecf3b1515f10
Catalin Stan <catalin.stan@prodigy-it-solutions.com>
2019-07-06 15:13:24 +0300

    initialize project

---
+++ src/main/java/com/ucode/demohotelmanagement/DemoHotelManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ucode.demohotelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHotelManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHotelManagementApplication.class, args);
+    }
+
+}
